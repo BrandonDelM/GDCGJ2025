81de5b243bf64bb10d4f406b52896413a53adcac
Zabil-Bin27 <jvan23@protonmail.ch>
2025-04-12 22:13:20 -0700

    Building class allows multiple buildings

--- building_component.gd
+++ building_component.gd
@@ -1,9 +1,9 @@
-class_name buildingcomponent
+class_name building
 extends Panel
 
-@export var MIN_COST := 10
-@export var MIN_BUILDINGS := 0
-@export var MONEY_GENERATION := 0.1
+@export var MIN_COST : int
+@export var MIN_AMOUNT : int
+@export var MONEY_GEN : float
 
 var building_cost : int
 var building_amount : int
@@ -14,35 +14,30 @@ var building_generation : float
 
 
 func _ready() -> void:
-	print("Building is working!")
 	building_cost = MIN_COST
-	building_amount = MIN_BUILDINGS
-	building_generation = MONEY_GENERATION
-	update_labels()
+	building_amount = MIN_AMOUNT
+	building_generation = MONEY_GEN
+	print("Building is working!")
+	update()
 
-func update_labels():
+func update():
 	cost_text.text = "$" + str(building_cost)
 	amount_text.text = str(building_amount)
+	
 
 func generate_money():
 	if building_amount > 0:
 		Global.money_value += building_generation * building_amount
-		print("Generating money!")
-	else:
-		print("Not generating money :(")
-	update_labels()
+	update()
 
 func _on_building_buy_button_pressed() -> void:
-	print("Processing building")
+	
 	if Global.money_value >= building_cost:
 		print("Building bought!")
 		Global.money_value -= building_cost
 		building_amount += 1
-		building_cost *= 1.1
-		update_labels()
-	else:
-		print("Building not bought")
-
+		building_cost *= 1.25
+		update()
 
 func _on_timer_timeout() -> void:
 	generate_money()
