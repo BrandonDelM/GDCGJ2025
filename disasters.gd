20e24b1b333e3b73030bd6ded29215b5da6bb3e7
Brandon D. <alyssajoseph415@gmail.com>
2025-04-12 18:09:53 -0700

    Potentially made the earthquake

--- disasters.gd
+++ disasters.gd
@@ -1,53 +1,50 @@
 extends StaticBody2D
 
 var disaster_state = "none"
-const disasters = ["acid", "storm"]
+const disasters = ["acid", "storm", "meteor", "earthquake"]
+var disaster_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$disaster_techs/acid_rain_tech/acid_rain.visible = false
-	$acid_tint.visible = false
-	$disaster_techs/lightning_tech/storm_rain.visible = false
-	$storm_tint.visible = false
-	if disaster_state == "none":
-		print("new game!")
-	elif disaster_state == "acid":
-		$disaster_techs/acid_rain_tech/acid_rain.visible = true
-		$acid_tint.visible = true
-	elif disaster_state == "storm":
-		$disaster_techs/lightning_tech/storm_rain.visible = true
-		$storm_tint.visible = true
-		thunder_strike()
-	pass # Replace with function body.
+	hide_all()
+	print("New game!")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$disaster_techs/acid_rain_tech/acid_rain.visible = false
-	$acid_tint.visible = false
-	$disaster_techs/lightning_tech/storm_rain.visible = false
-	$storm_tint.visible = false
+	hide_all()
 	if disaster_state == "acid":
 		$disaster_techs/acid_rain_tech/acid_rain.visible = true
 		$acid_tint.visible = true
 	elif disaster_state == "storm":
 		$disaster_techs/lightning_tech/storm_rain.visible = true
 		$storm_tint.visible = true
+	elif disaster_state == "meteor":
+		$disaster_techs/meteor_tech/meteor_storm.visible = true
+		$meteor_tint.visible = true
+	elif disaster_state == "earthquale":
+		Global.shake = "true"
 
 
 func _on_weather_timer_timeout():
 	if disaster_state == "none":
 		disaster_state = disasters[randi() % disasters.size()]
-		$weather_timer.wait_time = randi_range(60,120)
+		disaster_time = randi_range(60,120)
+		$weather_timer.wait_time = disaster_time
 		$weather_timer.start()
-		disaster_state = "acid"
 		if disaster_state == "storm":
 			print("thunder")
 			thunder_strike()
 		elif disaster_state == "acid":
 			print("acid")
+		elif disaster_state == "meteor":
+			print("meteor")
+		elif disaster_state == "earthquake":
+			print("earthquake")
+			
 	else:
 		disaster_state = "none"
+		Global.shake = "false"
 		$disaster_techs/lightning_tech/lightning_timer.stop()
 		$weather_timer.wait_time = 10
 		$weather_timer.start()
@@ -62,3 +59,11 @@ func thunder_strike():
 	$disaster_techs/lightning_tech/thunderFlash.play("lightning")
 	$disaster_techs/lightning_tech/lightning_timer.set_wait_time(nextWaitTime)
 	$disaster_techs/lightning_tech/lightning_timer.start()
+
+func hide_all():
+	$disaster_techs/acid_rain_tech/acid_rain.visible = false
+	$acid_tint.visible = false
+	$disaster_techs/lightning_tech/storm_rain.visible = false
+	$storm_tint.visible = false
+	$disaster_techs/meteor_tech/meteor_storm.visible = false
+	$meteor_tint.visible = false
--- global.gd
+++ global.gd
@@ -1,3 +1,3 @@
 extends Node
 
-var weather
+var shake : String = "false"
