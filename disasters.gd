61a70590f772c70b85080e07d2b3a7e2e1b332e7
Brandon D. <alyssajoseph415@gmail.com>
2025-04-12 17:38:00 -0700

    Added basic meteor shower event

--- disasters.gd
+++ disasters.gd
@@ -5,34 +5,34 @@ const disasters = ["acid", "storm"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$acid_rain_tech/acid_rain.visible = false
-	$acid_rain_tech/weather_tint.visible = false
-	$lightning_tech/storm_rain.visible = false
-	$lightning_tech/tint_layer/storm_tint.visible = false
+	$disaster_techs/acid_rain_tech/acid_rain.visible = false
+	$acid_tint.visible = false
+	$disaster_techs/lightning_tech/storm_rain.visible = false
+	$storm_tint.visible = false
 	if disaster_state == "none":
 		print("new game!")
 	elif disaster_state == "acid":
-		$acid_rain_tech/acid_rain.visible = true
-		$acid_rain_tech/weathertint.visible = true
+		$disaster_techs/acid_rain_tech/acid_rain.visible = true
+		$acid_tint.visible = true
 	elif disaster_state == "storm":
-		$lightning_tech/storm_rain.visible = true
-		$lightning_tech/tint_layer/storm_tint.visible = true
+		$disaster_techs/lightning_tech/storm_rain.visible = true
+		$storm_tint.visible = true
 		thunder_strike()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$acid_rain_tech/acid_rain.visible = false
-	$acid_rain_tech/weather_tint.visible = false
-	$lightning_tech/storm_rain.visible = false
-	$lightning_tech/tint_layer/storm_tint.visible = false
+	$disaster_techs/acid_rain_tech/acid_rain.visible = false
+	$acid_tint.visible = false
+	$disaster_techs/lightning_tech/storm_rain.visible = false
+	$storm_tint.visible = false
 	if disaster_state == "acid":
-		$acid_rain_tech/acid_rain.visible = true
-		$acid_rain_tech/weather_tint.visible = true
+		$disaster_techs/acid_rain_tech/acid_rain.visible = true
+		$acid_tint.visible = true
 	elif disaster_state == "storm":
-		$lightning_tech/storm_rain.visible = true
-		$lightning_tech/tint_layer/storm_tint.visible = true
+		$disaster_techs/lightning_tech/storm_rain.visible = true
+		$storm_tint.visible = true
 
 
 func _on_weather_timer_timeout():
@@ -40,7 +40,7 @@ func _on_weather_timer_timeout():
 		disaster_state = disasters[randi() % disasters.size()]
 		$weather_timer.wait_time = randi_range(60,120)
 		$weather_timer.start()
-		disaster_state = "storm"
+		disaster_state = "acid"
 		if disaster_state == "storm":
 			print("thunder")
 			thunder_strike()
@@ -48,7 +48,7 @@ func _on_weather_timer_timeout():
 			print("acid")
 	else:
 		disaster_state = "none"
-		$lightning_tech/lightning_timer.stop()
+		$disaster_techs/lightning_tech/lightning_timer.stop()
 		$weather_timer.wait_time = 10
 		$weather_timer.start()
 
@@ -59,6 +59,6 @@ func _on_lightning_timer_timeout():
 func thunder_strike():
 	print("BOOM!")
 	var nextWaitTime = randi() % 15 + 5
-	$lightning_tech/thunderFlash.play("lightning")
-	$lightning_tech/lightning_timer.set_wait_time(nextWaitTime)
-	$lightning_tech/lightning_timer.start()
+	$disaster_techs/lightning_tech/thunderFlash.play("lightning")
+	$disaster_techs/lightning_tech/lightning_timer.set_wait_time(nextWaitTime)
+	$disaster_techs/lightning_tech/lightning_timer.start()
